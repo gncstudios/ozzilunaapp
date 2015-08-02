67be8687e4096ce0e25eb76f2cbfb1c9b33b850c
Samuel Jacobs <samuel.jacobs@student.csulb.edu>
2015-08-02 00:22:25 -0700

    Set up profile buttno

--- ozziluna/client/templates/home/home.js
+++ ozziluna/client/templates/home/home.js
@@ -1,2 +1,13 @@
 Template.home.rendered = function() {
 };
+Template.home.helpers({
+  'thisDog': function() {
+    var thisDog = Dogs.findOne({username: Meteor.user().username});
+    if (thisDog) {
+        return thisDog;
+    }
+    else {
+      return false;
+    }
+  }
+});
